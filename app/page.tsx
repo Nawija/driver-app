a48351187e39bea6857a2275dc23d7034f509247
Valkon <96625039+Nawija@users.noreply.github.com>
2025-10-12 14:38:26 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -147,11 +147,14 @@ export default function HomePage() {
                         {Array.from({ length: 4 }).map((_, i) => (
                             <div
                                 key={i}
-                                className="animate-pulse bg-white p-5 rounded-2xl shadow-sm"
+                                className="animate-pulse bg-white p-4.5 rounded-xl shadow-sm border border-gray-100 flex justify-between items-center"
                             >
-                                <div className="h-6 w-1/3 bg-gray-200 rounded mb-2" />
-                                <div className="h-4 w-1/2 bg-gray-200 rounded mb-2" />
-                                <div className="h-20 w-full bg-gray-200 rounded" />
+                                <div className="flex items-center gap-2">
+                                    <div className="w-6 h-6 bg-gray-200 rounded-full" />
+                                    <div className="h-5 w-32 bg-gray-200 rounded" />
+                                </div>
+                                <div className="h-6 w-28 bg-gray-200 rounded-full" />
+                                <div/>
                             </div>
                         ))}
                     </div>
